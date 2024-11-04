bc890caf6158d3145beae7370cf68d391d230f9f
Eugene Kuzmich <indraminsk@gmail.com>
2024-11-04 13:58:35 +0100

    refactor: add wrap struct for default client

--- client.go
+++ client.go
@@ -1,16 +1,22 @@
-package redis
+package wredis
 
 import (
 	"fmt"
 	"github.com/redis/go-redis/v9"
 )
 
-func New(host, user, pass string, port, db, protocol int) *redis.Client {
-	return redis.NewClient(&redis.Options{
+type Client struct {
+	Instance *redis.Client
+}
+
+func New(host, user, pass string, port, db, protocol int) *Client {
+	instance := redis.NewClient(&redis.Options{
 		Addr:     fmt.Sprintf("%s:%d", host, port),
 		Username: user,
 		Password: pass,
 		DB:       db,
 		Protocol: protocol,
 	})
+
+	return &Client{Instance: instance}
 }
